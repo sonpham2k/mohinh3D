6420aaad7b254cb75b3f463619d787431ac32e21
sonpn-1267 <pham.ngoc.son@sun-asterisk.com>
2023-03-30 17:40:46 +0700

    Maintain text content in web

---
+++ js/content/index.js
@@ -0,0 +1,13 @@
+var index = {
+    about_me: `3D Thinking là tâm huyết của đội ngũ chuyên gia được đào tạo bài bản và có cơ hội thực
+                chiến không ít sự án tại Châu Âu và các dự án lớn tại Việt Nam. Năm 2022 đánh dấu bước
+                chuyển mình mạnh mẽ trong định hướng phát triển công ty. Thay đổi Thay slogan “Draw the
+                whole word” sang “No.1 3dprinting solutions” định hướng 3D Thinking là công ty số một
+                trong việc cung cấp giải pháp công nghệ in 3D cho thị trường Việt Nam. Hiện nay, với
+                hàng nghìn mã hàng và gần 100 mẫu máy in 3D các loại với các công nghệ in FDM, SLA, DLP,
+                LCD, SLS, SLM… 3D Thinking đang là công ty số một về cung cấp các giải pháp công nghệ in
+                3D.`,
+
+}
+
+document.getElementById('text_about_me').innerHTML = index.about_me;
